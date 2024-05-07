72681f4d0ffc131786dab886ed64f50b3b32bf0b
LuisHBeck <beck.henrique.luis@gmail.com>
2024-05-07 14:06:10 -0300

    feat: update primary color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/technical-content">
             Read the docs 📄
           </Link>
         </div>
